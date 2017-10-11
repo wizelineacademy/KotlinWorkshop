4b35c26658b35cd9848d381aeef29ad71a0b444b
Ademar Alves de Oliveira <ademar.oliveira@wizeline.com>
2017-10-11 13:17:55 -0500

    Solve the step-3

--- src/main/com/wizeline/workshop/Ticker.kt
+++ src/main/com/wizeline/workshop/Ticker.kt
@@ -7,6 +7,7 @@ class Ticker {
     @SerializedName("id")
     var id: String? = null
 
-    // TODO add the price variable that reads the json key price_usd, uses the id var above as example
+    @SerializedName("price_usd")
+    var price: String? = null
 
 }
