49ba64725d92c8fdcc14c0f18ab7ea7edca96694
Ademar Alves de Oliveira <ademar.oliveira@wizeline.com>
2017-10-11 13:13:50 -0500

    Solve the step 2

--- src/main/com/wizeline/workshop/Main.kt
+++ src/main/com/wizeline/workshop/Main.kt
@@ -1,4 +1,5 @@
 package com.wizeline.workshop
 
-// TODO create your main function ( fun main(args: Array<String>) )
-// TODO print a hello world and run it ( println("Hello World") )
+fun main(args: Array<String>) {
+    println("Hello World")
+}
