d9958524245e4829c86eebf4ed60cabdf9f89052
Ademar Alves de Oliveira <ademar.oliveira@wizeline.com>
2017-10-11 16:51:36 -0500

    Solve the step 7

--- src/main/com/wizeline/workshop/DollarBitcoinConverter.kt
+++ src/main/com/wizeline/workshop/DollarBitcoinConverter.kt
@@ -7,16 +7,18 @@ class DollarBitcoinConverter(
 ) {
 
     fun execute(dollar: String?): DollarBitcoinModel {
-        // TODO using the safe operator "?." call to toDoubleOrNull to get the double value from the dollar argument and save on a value named amount
-        // TODO using the smart cast check if the amount is a non null double: "if (amount is Double)"
-        // TODO if the amount is null throw an IllegalArgumentException with the message "Cannot convert $dollar"
-        // TODO if the amount is a valid non null double, call "getBitcoinTicker.execute()" to get a list of tickers to make the conversion
-        // TODO filter the ticker list to remove all null tickers and all tickers with null price: ".mapNotNull { it.price?.toDoubleOrNull() }"
-        // TODO filter again to remove values less than zero: .filter { it > 0.0 }
-        // TODO map the list to have the converted value: .map { amount / it }
-        // TODO map the list with converted values to our data class model: .map { DollarBitcoinModel(amount.toDollar(), it.toBitcoin()) }
-        // TODO get the first element of the list or null if after the filters we have no more elements: .firstOrNull()
-        // TODO uses the elvis operator "?:" to throw an NoSuchElementException with the message "No available ticker" if the pipe result is null
+        val amount = dollar?.toDoubleOrNull()
+        return if (amount is Double) {
+            getBitcoinTicker.execute()
+                    .mapNotNull { it.price?.toDoubleOrNull() }
+                    .filter { it > 0.0 }
+                    .map { amount / it }
+                    .map { DollarBitcoinModel(amount.toDollar(), it.toBitcoin()) }
+                    .firstOrNull()
+                    ?: throw NoSuchElementException("No available ticker")
+        } else {
+            throw IllegalArgumentException("Cannot convert $dollar")
+        }
     }
 
 }
