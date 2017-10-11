4b1f5331de63e8c54fc6fec8b51e77baa3a20e96
Ademar Alves de Oliveira <ademar.oliveira@wizeline.com>
2017-10-11 16:10:20 -0500

    Solve the step 4

--- src/main/com/wizeline/workshop/GetBitcoinTicker.kt
+++ src/main/com/wizeline/workshop/GetBitcoinTicker.kt
@@ -19,10 +19,12 @@ class GetBitcoinTicker(
                 .create(DataSource::class.java)
     }
 
-    // TODO create a function named "execute" that returns a List<Ticker>
-    // TODO inside the execute function uses the dataSource.getTickets using "bitcoin" as coin
-    // TODO the previous method returns a retrofit object, you need to call their execute() method an then call the body() method
-    // TODO at this point you have a nullable list, uses the safe call "?." to filter the list to have only bitcoin tickers: .filter { it.id == "bitcoin" }
-    // TODO uses the elvis operator to send an emptyList if you receive a null: ?: emptyList()
+    fun execute(): List<Ticker> {
+        return dataSource.getTickets("bitcoin")
+                .execute()
+                .body()
+                ?.filter { it.id == "bitcoin" }
+                ?: emptyList()
+    }
 
 }
