1f5b1eaf769c02a383bbf7b6623f6456c78ee21f
Ademar Alves de Oliveira <ademar.oliveira@wizeline.com>
2017-10-11 16:25:15 -0500

    Solve the step 6

--- src/main/com/wizeline/workshop/DoubleExt.kt
+++ src/main/com/wizeline/workshop/DoubleExt.kt
@@ -6,5 +6,6 @@ fun Double.toDollar(): String {
     return DecimalFormat("$ ###,###,##0.00").format(this)
 }
 
-// TODO see the previous function, it is an extension to Double class named "toDollar()"
-// TODO create the second extension to Double class named "toBitcoin" using the pattern "0.00000000 BTC"
+fun Double.toBitcoin(): String {
+    return DecimalFormat("0.00000000 BTC").format(this)
+}
