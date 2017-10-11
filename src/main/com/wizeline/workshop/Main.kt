6f652a1185c37222d5c63f56955170effd9ebe9b
Ademar Alves de Oliveira <ademar.oliveira@wizeline.com>
2017-10-11 17:02:53 -0500

    Solve the step 8

--- src/main/com/wizeline/workshop/Main.kt
+++ src/main/com/wizeline/workshop/Main.kt
@@ -1,11 +1,13 @@
 package com.wizeline.workshop
 
 fun main(args: Array<String>) {
-    // TODO print the question: "How much dollars do you want to convert?"
-    // TODO read the user input with "readLine()" and save it on a value
-    // TODO print the message "Please wait meanwhile I convert it"
-    // TODO create a DollarBitcoinConverter instance
-    // TODO call the "execute" function sending the user input and save the return into a value named model
-    // TODO print the follow message: "${model.dollar} dollars is equal to ${model.bitcoin}"
-    // TODO put the "execute" call inside a try/catch and if there is an exception print: "Error ${e.message}"
+    println("How much dollars do you want to convert?")
+    val input = readLine()
+    println("Please wait meanwhile I convert it")
+    try {
+        val model = DollarBitcoinConverter().execute(input)
+        println("${model.dollar} dollars is equal to ${model.bitcoin}")
+    } catch (e: Exception) {
+        println("Error ${e.message}")
+    }
 }
